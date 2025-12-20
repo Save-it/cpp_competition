dcd2b21a2cf464bb0aa07e4e27134802a50d9811
Save-it <lin2011746853@outlook.com>
2025-12-20 13:17:43 +0000

    12/20

--- luogu/P3952 [NOIP 2017 提高组] 时间复杂度.cpp
+++ luogu/P3952 [NOIP 2017 提高组] 时间复杂度.cpp
@@ -2,36 +2,69 @@
 using namespace std;
 const int MAXT = 20;
 const int MAXL = 200;
+struct bi;
 int t, L[MAXT], F[MAXT], E[MAXT];
-string O[MAXT], tmp;
-char b;
+string inO[MAXT], O[MAXT], tmp;
+
 struct bi{
     char name;
     int num;
 };
+bi b;
+vector<bi> B;
 
-int findele(char a[], char ele) {
-    for (char i : a) if (i == ele) return 1;
-    return 0;
+string calcO(string o, int num, bool n) {
+    string out = "O(";
+    if (n) {
+        out = "O(n^" + to_string(atoi(o.substr(4, o.size() - 5).c_str()) + num);
+    } else if (o != "") out += (char(num * atoi(o.substr(2, o.size() - 2).c_str())) + '0');
+    out += ')';
+    return out;
+}
+
+bool findele(vector<bi> a, bi ele) {
+    for (bi i : a) if (i.name == ele.name) return true;
+    return false;
 }
 
 int main() {
     cin >> t;
 
     for (int i = 0; i < t; i++) {
-        cin >> L[i] >> O[i];
+        cin >> L[i] >> inO[i];
+        B.clear();
         for (int j = 0; j < L[i]; j++) {
             getline(cin, tmp);
             if (tmp[0] == 'F') {
                 F[i]++;
-                tmp = tmp.substr(1, tmp.size() - 2);
-                b = tmp[2];
-                if(findele())
+                b.name = tmp[2];
+                tmp = tmp.substr(4, tmp.size() - 4);
+                if(findele(B, b)) {
+                    cout << "B ERR" << endl;
+                    break;
+                } else {
+                    B.push_back(b);
+                    if(int(tmp[5]) >= 48 && int(tmp[5]) <= 57) {
+                        b.num = atoi(tmp.substr(4, tmp.find(" ")).c_str());
+                        if(tmp[tmp.size() - 1] == 'n') O[i] = calcO(O[i], 1, true);
+                        else {
+                            calcO(O[i], atoi(tmp.substr(tmp.find(" ") + 1, tmp.size() - tmp.find(" ") - 1).c_str()), false);
+                        }
+                    } else {
+                        O[i] = calcO(O[i], 1, false);
+                        break;
+                    }
+                }
             }
-            if (tmp[0] == 'E') E[i]++;
-
+            else (tmp[0] == 'E') E[i]++;
         }
+        if (F[i] != E[i]) {
+            cout << F[i] << " " << E[i] << " FE ";
+            cout << "ERR" << endl;
+        }
+        else if (O[i] == inO[i]) cout << "Yes" << endl;
+        else cout << "No" << endl;
     }
 
     return 0;
-}
+} 
