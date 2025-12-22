13a5f2e4bed5f3aa82cd359f8cc1983e9e381a69
Save-it <lin2011746853@outlook.com>
2025-12-22 09:54:22 +0000

    12/22

--- P1083.cpp
+++ P1083.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 const int MAXN = 2000000;
 const int MAXM = 2000000;
-int n, m, in[MAXN], re[MAXN], d, s, t;
+long long n, m, in[MAXN], re[MAXN], d, s, t;
 
 int main() {
     cin >> m >> m;
@@ -12,8 +12,8 @@ int main() {
     
     for (int i = 0; i < m; i++) {
         cin >> d >> s >> t;
-        for (int j = d - 1; j < t; j++) {
-                in[j] -= s;
+        for (int j = s - 1; j < t; j++) {
+                in[j] -= d;
                 if (in[j] < 0) {
                     cout << -1 << endl << i + 1;
                     return 0;
