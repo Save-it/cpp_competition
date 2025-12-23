4590a998098b9cee07f52b7f00a8f604a922313c
Save-it <lin2011746853@outlook.com>
2025-12-23 04:56:32 +0000

    12/23

--- P1083.cpp
+++ P1083.cpp
@@ -6,16 +6,16 @@ long long n, m, in[MAXN], re[MAXN], d, s, t;
 
 int main() {
     cin >> m >> m;
-    cin >> in[0];
-    re[0] = in[0];
-    for (int i = 1; i < n; i++) cin >> in[i];
+    cin >> in[1];
+    re[1] = in[1];
+    for (int i = 2; i < n; i++) cin >> in[i];
     
-    for (int i = 0; i < m; i++) {
+    for (int i = 1; i <= m; i++) {
         cin >> d >> s >> t;
-        for (int j = s - 1; j < t; j++) {
+        for (int j = s; j <= t; j++) {
                 in[j] -= d;
                 if (in[j] < 0) {
-                    cout << -1 << endl << i + 1;
+                    cout << -1 << endl << i;
                     return 0;
                 }
         }
