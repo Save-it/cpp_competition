05cb1fdac9dfed3ea1a964aa59c5944ebaf86871
Save-it <lin2011746853@outlook.com>
2025-12-24 08:47:52 +0000

    12/24

--- P1083.cpp
+++ P1083.cpp
@@ -2,36 +2,52 @@
 using namespace std;
 const int MAXN = 1000000;
 const int MAXM = 1000000;
-int n, m, in[MAXN], d, s, t;
+int n, m, in[MAXN], ch[MAXN], d, s, t;
+
+struct segmentTree {
+    int l, r;
+    int dat;
+} T[MAXN * 4];
+
+void build(int p, int l, int r) {
+    T[p].l = l;
+    T[p].r = r;
+    if (l == r) {
+        T[p].dat = ch[p];
+        return;
+    }
+
+    int mid = (l + r) / 2;
+    build(p * 2, l, mid);
+    build(p * 2 + 1, mid + 1, r);
+
+    T[p].dat = T[p * 2].dat + T[p * 2 + 1].dat;
+}
+
+void change(int idx, int pos, int val) {
+    if (T[idx].l == T[idx].r) {
+        T[idx].dat += val;
+        return;
+    }
+
+    if ()
+}
 
 int main() {
     cin >> n >> m;
-    int l[n + 1][n + 1];
-    for (int i = 1; i <= n; i++) fill(l[i], l[i] + n + 1, 999999999);
-    for (int i = 1; i <= n; i++) {
+    cin >> in[1];
+    ch[1] = in[1];
+    for (int i = 2; i < n; i++) {
         cin >> in[i];
-        for (int j = n; j >= i; j--) {
-            l[i][j] = min(l[i][j - 1], in[i]);
-        }
-    }
-    
-    for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
-            cout << l[i][j] << " ";
-        }
-        cout << endl;
+        ch[i] = in[i] - in[i - 1];
     }
 
-    // for (int i = 1; i <= m; i++) {
-    //     cin >> d >> s >> t;
-    //     for (int j = s; j <= t; j++) {
-    //         in[j] = in[j] - d;
-    //         if (in[j] < 0) {
-    //             cout << -1 << endl << i;
-    //             return 0;
-    //         }
-    //     }
-    // }
+    for (int i = 1; i <= m; i++) {
+        cin >> d >> s >> t;
+        ch[s] -= d;
+        ch[t + 1] += d;
+    }
+
     cout << 0;
 
     return 0;
